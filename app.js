ee23292eaa7351e3b684c57edb99908ed5a3a197
shubhangiBhapkar <shubhangibhapkar35@gmail.com>
2024-09-24 21:54:13 +0530

    Boilerplate

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const mongoose=require("mongoose");
 const listing=require("./models/listing");
 const path = require('path');
 const methodOverride=require("method-override")
+const ejsMate=require("ejs-mate");
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 MONGO_URL="mongodb://127.0.0.1:27017/wonderlust";
 main().then(()=>{
