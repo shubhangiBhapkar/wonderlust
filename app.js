9dff6a89fc46a80e506c66b6070f52d012241222
web Development <shubhangibhapkar35@gmail.com>
2024-10-15 14:15:37 +0530

    cookie set

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const session=require("express-session");
 // const{listingSchema,reviewSchema}=require("./schema.js");
 // const Review=require("./models/review.js");
 
@@ -19,6 +20,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+
+const sessionOptions={
+    secret:"mysupersecretecode",
+    resave:false,
+    saveUninitialized:true,
+    cookie :{
+        expires:Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly:true
+    }
+};
+app.use(session(sessionOptions));
 
 MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
 main().then(() => {
