023b19ad4988f7cc3951b546a6a39ccfcdf5d385
web Development <shubhangibhapkar35@gmail.com>
2024-10-17 13:44:04 +0530

    user signup

--- Route/user.js
+++ Route/user.js
@@ -1,18 +1,25 @@
-const express=require("express");
+const express = require("express");
 const wrapAsync = require("../utils/wrapAsync");
-const router=express.Router();
-const User=require("../models/user.js");
+const router = express.Router();
+const User = require("../models/user.js");
 
-router.get("/signup",(req,res)=>{
+router.get("/signup", (req, res) => {
    res.render("users/signup.ejs")
 });
 
-router.post("/signup",wrapAsync(async(req,res)=>{
-   let {username,email,password}=req.body;
-   const newUser=new User({username,email});
 
-   let registerUser= await User.register(newUser,password);
-   req.flash("success","Welcome to wanderlust!");
-   res.redirect("/listing");
-}));
-module.exports=router;
+router.post("/signup", async (req, res) => {
+   try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ username, email });
+
+      let registerUser = await User.register(newUser, password);
+      req.flash("success", "Welcome to wanderlust!");
+      res.redirect("/listing");
+
+   } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
+   }
+});
+module.exports = router;
